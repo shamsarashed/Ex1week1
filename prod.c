8569b48d35bb40bae726668671ad416ee38f06d1
Hessa <Hessaleisaei@gmail.com>
2021-06-28 21:39:36 +0400

    Mygreeter12 function to get the sum of the two variables a and b

---
+++ prod.c
@@ -0,0 +1,11 @@
+#include "prod.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void my_greeter12(int a, int b)
+{
+
+    int sum = a+b;
+    printf("Hello world, your sum is:%d\n", sum);
+}
