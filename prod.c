06b3d7634a126411f730df7fe4aeb5b6ea920653
shamsarashed <86327424+shamsarashed@users.noreply.github.com>
2021-06-30 12:49:08 +0400

    Add files via upload
    Modified the code

--- prod.c
+++ prod.c
@@ -5,7 +5,9 @@
 
 void my_greeter12(int a, int b)
 {
-
-    int sum = a+b;
+    int sum;
+    printf("Enter two numbers:");
+    scanf("%d %d", &a, &b);
+    sum = a+b;
     printf("Hello world, your sum is:%d\n", sum);
 }
